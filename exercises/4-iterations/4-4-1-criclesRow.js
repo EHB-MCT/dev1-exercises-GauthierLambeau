2b9ded34b21141a8d829df1043abd96164b39896
Gauthier Lambeau <gauthier.lambeau@student.ehb.be>
2022-10-26 12:16:13 +0200

    row of circle

---
+++ exercises/4-iterations/4-4-1-criclesRow.js
@@ -0,0 +1,23 @@
+"use strict";
+
+import context from "../../scripts/context.js";
+import * as Utils from "../../scripts/utils.js";
+
+
+
+context.fillStyle= 'LightSkyBlue';
+context.strokeStyle= 'LightSkyBlue';
+strokeCircle();
+
+function strokeCircle() {  
+for (let j = 0; j <10; j++){
+    for (let i = 0; i<10; i++){
+    Utils.strokeCircle(0 + (i * 82),0, 40);
+        }
+    }
+    for (let j = 0; j <=10; j++){
+        for (let i = 0; i<10; i++){
+        Utils.strokeCircle(0 + (i * 82),0 + (j*82), 40);
+            }
+        }
+}
